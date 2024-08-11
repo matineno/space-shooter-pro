d9234f3febdd60f93ad290d35f395864f58eb8b6
Martins Adogamhe <martinsadogamhe9@gmail.com>
2024-08-10 23:09:17 -0500

    added player boundaries with wrap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,6 +35,34 @@ public class Player : MonoBehaviour
 
         transform.Translate(direction * _speed * Time.deltaTime);
 
+        // PLAYER BOUNDARIES ON THE Y AXIS
+        //if player position on the y is greater than 0
+        //y position = 0
+        //else if player position on the y is less than -3.8
+        //y position = -3.8
+
+        if (transform.position.y >=0) 
+        {   
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        // PLAYER BOUNDARIES ON THE X AXIS with wrap around
+        //if player position on the x is greater than 10.25
+        //x position = -10.25
+        //else if player position on the x is less than -10.25
+        //x position = 10.25
+        if (transform.position.x >= 10.25f) 
+        {
+            transform.position = new Vector3(-10.25f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -10.25f)
+        {
+            transform.position = new Vector3(10.25f, transform.position.y, 0);
+        }
 
     }
 }
