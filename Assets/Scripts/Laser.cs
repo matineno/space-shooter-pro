ac7b0c508cdaedc530644445d7c0a3ff44201709
Martins Adogamhe <martinsadogamhe9@gmail.com>
2024-08-11 10:26:03 -0500

    added behaviour to laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //speed variable of 8
+    [SerializeField]
+    private float _speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translate laser up
+        Vector3 direction = new Vector3(0, 1, 0);
+        //translate laser up at 8 meters per second in real time
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
+    }
+}
