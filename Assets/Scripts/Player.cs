a7e093b38b926f3acea7bd9c9e26f285ffce574d
Martins Adogamhe <martinsadogamhe9@gmail.com>
2024-08-11 12:46:33 -0500

    added firelaser method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
 
 
     // Start is called before the first frame update
@@ -26,14 +29,7 @@ public class Player : MonoBehaviour
     void Update() 
     {
         CalculateMovement();
-
-        //if i hit the space key
-        //spawn gameObject
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
-        }
+        FireLaser();
 
     }
 
@@ -69,6 +65,18 @@ public class Player : MonoBehaviour
         }
         else if (transform.position.x <= -10.25f) {
             transform.position = new Vector3(10.25f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser() 
+    {
+        //if i hit the space key
+        //spawn gameObject
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire) {
+
+            _canFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
 }
