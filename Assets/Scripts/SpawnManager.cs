84c71e903801a9d6dfa66529b613d427160ac03a
Martins Adogamhe <martinsadogamhe9@gmail.com>
2024-08-16 23:33:58 -0500

    finiahed with spawning-manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,6 +12,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyContainer;
 
+    private bool _stopSpawning = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,12 +40,17 @@ public class SpawnManager : MonoBehaviour
         //yield return null; //wait 1 frame
         //yield return new WaitForSeconds(5.0f); //wait 5 seconds
 
-        while (true) 
+        while (_stopSpawning == false) 
         {
             Vector3 posTosSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posTosSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(waitTime);
         };
+    }
+
+    public void OnPlayerDeath() 
+    {
+        _stopSpawning = true;
     }
 }
